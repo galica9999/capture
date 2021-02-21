eb2d113a9c5cdcc86d1b34d915c8aa39321f50d0
galica9999 <galica9999@gmail.com>
2021-02-20 22:14:18 -0500

    added framer motion and animated page loading

--- src/components/AboutSection.js
+++ src/components/AboutSection.js
@@ -1,6 +1,6 @@
 import React from "react";
-import styled from "styled-components";
 import { About, Description, Image, Hide } from "../styles";
+import { motion, motionn } from "framer-motion";
 //images
 import home1 from "../img/home1.png";
 
@@ -8,19 +8,19 @@ const AboutSection = () => {
     return (
         <About>
             <Description>
-                <div className="title">
+                <motion.div className="title">
                     <Hide>
-                        <h2>We work to make</h2>
+                        <motion.h2>We work to make</motion.h2>
                     </Hide>
                     <Hide>
-                        <h2>
+                        <motion.h2>
                             your <span>dreams</span> come
-                        </h2>
+                        </motion.h2>
                     </Hide>
                     <Hide>
-                        <h2>true</h2>
+                        <motion.h2>true</motion.h2>
                     </Hide>
-                </div>
+                </motion.div>
                 <p>
                     Contact us for any photography or videography ideas that you
                     have. We have professionals with amazinng skills.
--- src/pages/MovieDetail.js
+++ src/pages/MovieDetail.js
@@ -2,6 +2,8 @@ import React, { useState, useEffect } from "react";
 import styled from "styled-components";
 import { useHistory } from "react-router-dom";
 import { MovieState } from "../movieState";
+import { motion } from "framer-motion";
+import { pageAnimation } from "../animation";
 
 const MovieDetail = () => {
     const history = useHistory();
@@ -19,7 +21,12 @@ const MovieDetail = () => {
     return (
         <>
             {movie && (
-                <Details>
+                <Details
+                    exit="exit"
+                    variants={pageAnimation}
+                    initial="hidden"
+                    animate="show"
+                >
                     <Headline>
                         <h2>{movie.title}</h2>
                         <img src={movie.mainImg} alt="movie" />
@@ -42,7 +49,7 @@ const MovieDetail = () => {
     );
 };
 
-const Details = styled.div`
+const Details = styled(motion.div)`
     color: white;
 `;
 const Headline = styled.div`
