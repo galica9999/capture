33593f243dd2cbb5fb754ac5dcee89342cee83ce
galica9999 <galica9999@gmail.com>
2021-02-20 22:27:22 -0500

    added animations for about section

--- src/components/AboutSection.js
+++ src/components/AboutSection.js
@@ -1,6 +1,7 @@
 import React from "react";
 import { About, Description, Image, Hide } from "../styles";
-import { motion, motionn } from "framer-motion";
+import { motion } from "framer-motion";
+import { titleAnim, fade, photoAnim } from "../animation";
 //images
 import home1 from "../img/home1.png";
 
@@ -10,25 +11,31 @@ const AboutSection = () => {
             <Description>
                 <motion.div className="title">
                     <Hide>
-                        <motion.h2>We work to make</motion.h2>
+                        <motion.h2 variants={titleAnim}>
+                            We work to make
+                        </motion.h2>
                     </Hide>
                     <Hide>
-                        <motion.h2>
+                        <motion.h2 variants={titleAnim}>
                             your <span>dreams</span> come
                         </motion.h2>
                     </Hide>
                     <Hide>
-                        <motion.h2>true</motion.h2>
+                        <motion.h2 variants={titleAnim}>true</motion.h2>
                     </Hide>
                 </motion.div>
-                <p>
+                <motion.p variants={fade}>
                     Contact us for any photography or videography ideas that you
                     have. We have professionals with amazinng skills.
-                </p>
-                <button>Contact Us</button>
+                </motion.p>
+                <motion.button variants={fade}>Contact Us</motion.button>
             </Description>
             <Image>
-                <img src={home1} alt="guy with camera" />
+                <motion.img
+                    variants={photoAnim}
+                    src={home1}
+                    alt="guy with camera"
+                />
             </Image>
         </About>
     );
